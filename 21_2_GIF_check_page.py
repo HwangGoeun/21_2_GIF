43612537bb71a605de22e3760f2f88f9cd8eada6
HwangGoeun <hwang_gim@naver.com>
2021-11-09 19:06:49 +0900

    옷 정보 저장 페이지 제작 시작

---
+++ 21_2_GIF_check_page.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+###################################################
+
+# GUI 화면 설정
+win = Tk() # GUI 생성 
+win.title("21_2_GIF_moving2") #상단의 타이틀 지정
+win.geometry("640x640") # 크기 설정 (640x640)
+
+win.mainloop() # GUI가 보이고 종료될때까지 실행함
