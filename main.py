f0a127dfe3a5dfea7ee5180d835e941b9a5f0939
HwangGoeun <hwang_gim@naver.com>
2021-11-12 16:30:33 +0900

    위젯 요소들 라즈베리파이 해상도에 맞춤

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ GUI 화면 설정
 
 win = Tk() # GUI 생성 
 win.title("21_2_GIF_moving2") #상단의 타이틀 지정
-win.geometry("640x640") # 크기 설정 (640x640)
+win.geometry("500x200") # 크기 설정
 ################################################################################################
 '''
 startFrame(시작 화면) 코드
@@ -144,10 +144,10 @@ startFrame = Frame(win) #시작 화면 설정 화면 프레임
 
 addBtn = Button(startFrame, text = "옷 정보 추가하기", command = go_infoFrame)
 recBtn = Button(startFrame, text = "옷 추천 받기", command = go_motorFrame)
-addBtn.config(width = 50, height = 5)
-recBtn.config(width = 50, height = 5)
+addBtn.config(width = 50, height = 3)
+recBtn.config(width = 50, height = 3)
 
-addBtn.grid(pady=100)
+addBtn.grid(pady=10)
 recBtn.grid(pady=10)
 
 startFrame.pack()
@@ -158,7 +158,8 @@ infoFrame(옷 정보 설정 화면) 코드
 
 #프레임 설정
 infoFrame = Frame(win) # 옷 정보 설정 화면 프레임
-
+seasonFrame = Frame(infoFrame) # 계절 설정 화면 프레임
+weatherFrame = Frame(infoFrame) # 날씨 설정 화면 프레임
 
 # 체크박스 상태 확인용 변수 설정
 # 1 = 계절 / 2 = 날씨 (예: checkVar1n은 계절 관련, checkVar2n은 날씨 관련)
@@ -173,28 +174,31 @@ checkVar24 = IntVar() # 비 올 때
 
 # 체크 박스 출력
 # 계절 설정 체크 박스
-lab1 = Label(infoFrame, text = "무슨 계절에 입나요?")
-c11 = Checkbutton(infoFrame, text = "봄, 가을", variable = checkVar11, command = c11_check)
-c12 = Checkbutton(infoFrame, text = "여름", variable = checkVar12, command = c12_check)
-c13 = Checkbutton(infoFrame, text = "겨울", variable = checkVar13, command = c13_check)
+lab1 = Label(seasonFrame, text = "무슨 계절에 입나요?")
+c11 = Checkbutton(seasonFrame, text = "봄, 가을", variable = checkVar11, command = c11_check)
+c12 = Checkbutton(seasonFrame, text = "여름", variable = checkVar12, command = c12_check)
+c13 = Checkbutton(seasonFrame, text = "겨울", variable = checkVar13, command = c13_check)
 
 lab1.pack()
 c11.pack()
 c12.pack()
 c13.pack()
 
-# 계절 설정 체크 박스
-lab2 = Label(infoFrame, text = "어떨 때 입고 싶은가요?")
-c21 = Checkbutton(infoFrame, text = "더울 때", variable = checkVar21, command = c21_check)
-c22 = Checkbutton(infoFrame, text = "추울 때", variable = checkVar22, command = c22_check)
-c23 = Checkbutton(infoFrame, text = "보통 때", variable = checkVar23, command = c23_check)
-c24 = Checkbutton(infoFrame, text = "비 올 때", variable = checkVar24, command = c24_check)
+# 날씨 설정 체크 박스
+lab2 = Label(weatherFrame, text = "어떨 때 입고 싶은가요?")
+c21 = Checkbutton(weatherFrame, text = "더울 때", variable = checkVar21, command = c21_check)
+c22 = Checkbutton(weatherFrame, text = "추울 때", variable = checkVar22, command = c22_check)
+c23 = Checkbutton(weatherFrame, text = "보통 때", variable = checkVar23, command = c23_check)
+c24 = Checkbutton(weatherFrame, text = "비 올 때", variable = checkVar24, command = c24_check)
 
 lab2.pack()
 c21.pack()
 c22.pack()
 c23.pack()
 c24.pack()
+
+seasonFrame.pack(side = LEFT)
+weatherFrame.pack(side = RIGHT)
 
 ################################################################################################
 '''
@@ -208,7 +212,7 @@ motorFrame = Frame(win) # 옷 정보 설정 화면 프레임
 mtBtn = Button(motorFrame, text = "UP", command = motor_up)
 mtBtn.config(width=30, height=5)
 
-mtBtn.grid(pady = 250)
+mtBtn.grid(pady = 20)
 '''
 startFrame 가는 버튼
 '''
